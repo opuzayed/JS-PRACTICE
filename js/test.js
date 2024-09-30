3dd2b296ac300a42d448198b184ca8f5112cda4f
zayed opu <zayedopu4933@gmail.com>
2024-09-30 12:53:29 +0600

    use forEach loop

--- js/test.js
+++ js/test.js
@@ -1,8 +1,18 @@
-const fruits = ['apple', 'banana', 'pineapple', 'watermelon'];
-let fLength = fruits.length;
- let text = '<ul>';
- for(i=0; i<fLength; i++){
-    text += '<li>' + fruits[i] + '</li>';
- }
- text += '</ul>';
-document.getElementById("demo").innerHTML = text;
+// const fruits = ['apple', 'banana', 'pineapple', 'watermelon'];
+// let fLength = fruits.length;
+//  let text = '<ul>';
+//  for(i=0; i<fLength; i++){
+//     text += '<li>' + fruits[i] + '</li>';
+//  }
+//  text += '</ul>';
+// document.getElementById("demo").innerHTML = text;
+
+const fruits = ['apple', 'banana', 'melon', 'pen', 'book'];
+let text= '<ul>';
+function myFunction (value)
+{
+    text += '<li>' + value + '</li>'
+}
+text += '</ul>';
+fruits.forEach(myFunction);
+document.getElementById('demo').innerHTML = text;
