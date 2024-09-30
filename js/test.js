1faecbd1ae5ba54322049a9ef3e8bbc99fe0122f
zayed opu <zayedopu4933@gmail.com>
2024-09-30 12:28:44 +0600

    use for loop

--- js/test.js
+++ js/test.js
@@ -1,1 +1,8 @@
-console.log("hello");
+const fruits = ['apple', 'banana', 'pineapple', 'watermelon'];
+let fLength = fruits.length;
+ let text = '<ul>';
+ for(i=0; i<fLength; i++){
+    text += '<li>' + fruits[i] + '</li>';
+ }
+ text += '</ul>';
+document.getElementById("demo").innerHTML = text;
