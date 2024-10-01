3c8fdeb8eb470c0d295b0ebad04474aebc80fce5
zayed opu <zayedopu4933@gmail.com>
2024-10-01 16:28:30 +0600

    find unique number from an array

--- js/test.js
+++ js/test.js
@@ -1,7 +1,7 @@
-// find duplicate number from arrayco
+// find unique number from array
 const numbers = ['2', '4', '5', '4', '5', '6', '7','6'];
 const duplicateNumbers = numbers.filter(function(value, index, array){
-    return array.indexOf(value) !== index;
+    return array.indexOf(value) === index;
 });
 console.log(duplicateNumbers);
 
