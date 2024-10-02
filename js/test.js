791a7fa6b4a418ca50aa48de95e9cee4bda26c67
zayed opu <zayedopu4933@gmail.com>
2024-10-02 15:56:57 +0600

    use for in loop

--- js/test.js
+++ js/test.js
@@ -1,9 +1,8 @@
-// find unique number from array
-const numbers = ['2', '4', '5', '4', '5', '6', '7','6'];
-const duplicateNumbers = numbers.filter(function(value, index, array){
-    return array.indexOf(value) === index;
-});
-console.log(duplicateNumbers);
+const person = {fName : 'opu', lName : 'zayed', Age: 34}
+for(let x in person) 
+{
+    console.log(person[x]);
+}
 
 
 
