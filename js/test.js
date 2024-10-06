c04851c32e4e4e160dc28a3811b378884ec69c75
zayed opu <zayedopu4933@gmail.com>
2024-10-06 21:59:11 +0600

    count a name how many use a sentence

--- js/test.js
+++ js/test.js
@@ -1,8 +1,4 @@
-const person = {fName : 'opu', lName : 'zayed', Age: 34}
-for(let x in person) 
-{
-    console.log(person[x]);
-}
-
-
-
+const sentence = 'i am opu.i am opu.i am opu';
+const matches = sentence.match(/opus/gi);
+const count = matches ? matches.length : 0;
+console.log(count);
