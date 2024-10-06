fc1e5c4badf2c334d24c869f93d8d0f0282ebdcf
zayed opu <zayedopu4933@gmail.com>
2024-10-06 22:12:59 +0600

    first time find index in name

--- js/test.js
+++ js/test.js
@@ -2,3 +2,9 @@ const sentence = 'i am opu.i am opu.i am opu';
 const matches = sentence.match(/opus/gi);
 const count = matches ? matches.length : 0;
 console.log(count);
+
+// 2nd part
+
+let position = sentence.search(/opu/i);
+position = position >= 0 ? position : 'not found';
+console.log(position);
