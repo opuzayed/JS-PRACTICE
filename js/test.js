ada3c74f097fc1064b193b8aa9ade72b10e1c9b9
zayed opu <zayedopu4933@gmail.com>
2024-10-06 22:49:22 +0600

    MATCH FIRST ELEMNT AND RETURN INDEX

--- js/test.js
+++ js/test.js
@@ -1,10 +1,10 @@
-const sentence = 'i am opu.i am opu.i am opu';
-const matches = sentence.match(/opus/gi);
-const count = matches ? matches.length : 0;
-console.log(count);
-
-// 2nd part
-
-let position = sentence.search(/opu/i);
-position = position >= 0 ? position : 'not found';
-console.log(position);
+function linearSearch(arr, val){
+    const length = arr.length;
+    for(let i = 0; i < length; i++){
+        if(arr[i] === val){
+            return i;
+        }
+    }
+    return 'not found';
+}
+console.log(linearSearch(['a','b', 'c', 'd', 'c'], 'c'));
