55a9daf6adb87ed62fe78128ed9e327b9b709a34
zayed opu <zayedopu4933@gmail.com>
2024-10-10 12:33:38 +0600

    all things are done for miving animate div

--- js/test.js
+++ js/test.js
@@ -1,21 +1,16 @@
-const obj = {
-  a: "lws",
-  b: undefined,
-  c: "learn with sumit",
-  d: false,
-  e: "",
-  f: "apple",
-  g: 40,
-  h: "k",
-  i: true,
-  j: NaN,
-};
-const truthyObject = function(obj){
- for(let i in obj){
-    if(!obj[i]){
-        delete obj[i];
+function moveOn() {
+  const animate = document.getElementById("animate");
+  let position = 0;
+  interval = setInterval(frame, 5);
+  function frame() {
+    if (position < 350) {
+      position++;
+      animate.style.top = position + "px";
+      animate.style.left = position + "px";
     }
- }
- return obj;
+    else{
+      clearInterval(interval);
+    }
+  }
 }
-console.log(truthyObject(obj));
+
