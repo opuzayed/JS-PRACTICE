4255015f4840941563620c9419753b4dbdf70248
zayed opu <zayedopu4933@gmail.com>
2024-10-30 21:35:37 +0600

    ternary operator simple example

--- js/test.js
+++ js/test.js
@@ -1,12 +1,2 @@
-const searchInput = document.querySelector(".search");
-const display = document.querySelector(".result");
-const thanks = document.querySelector(".thanks");
-
-function show(){
-  display.innerHTML = this.value;
-  let self = this;
-  setTimeout(function(){
-    thanks.innerHTML = `You Have Typed ${self.value}`;
-  }, 1000)
-}
-searchInput.addEventListener("keyup", show);
+let age = 18;
+let type = (age >= 18) ? 'adult' : 'child';
