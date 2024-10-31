6b42544635c9e1959b52a0b26f6a98dc09afad03
zayed opu <zayedopu4933@gmail.com>
2024-10-31 15:50:46 +0600

    spread operator example

--- js/test.js
+++ js/test.js
@@ -1,9 +1,3 @@
-const myObj = {
-        name: 'opu',
-        age: 26,
-        estd: 'hero'
-};
-const keys = Object.keys(myObj);
-const values = Object.values(myObj);
-const entries = Object.entries(myObj);
-console.log(entries);
+const numbers = [1, 2, 3, 4, 5];
+const newNumbers = [...numbers, 6, 7, 8];
+console.log(newNumbers);
