765c2476a5be54d929a0737dbeb86dbda8ffba7b
zayed opu <zayedopu4933@gmail.com>
2024-10-31 12:32:25 +0600

    push  method example

--- js/test.js
+++ js/test.js
@@ -1,4 +1,3 @@
-const numbers1 = [1, 2, 3, 4, 5, 6, 7];
-const numbers2 = [2, 4, 6, 8, 10, 12, 14, 16];
-const result = numbers1.concat(numbers2);
-console.log(result);
+const numbers = [2, 4, 6, 8];
+const result = numbers.push(12, 14);
+console.log('result = ',result);
