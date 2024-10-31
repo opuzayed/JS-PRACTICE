ef9beebfc19418f5f288f8a3dca99d113eafd3a9
zayed opu <zayedopu4933@gmail.com>
2024-10-31 10:52:41 +0600

    slice method example

--- js/test.js
+++ js/test.js
@@ -1,5 +1,3 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7];
-const result = numbers.filter((currentValue, currentIndex, array) => {
-return currentValue > 4;
-});
+const numbers = [2, 4, 5, 6, 7, 8];
+const result = numbers.slice(1, 3);
 console.log(result);
