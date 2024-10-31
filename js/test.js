df9e89c56cd0f097c4792b53a11e6d9410706baf
zayed opu <zayedopu4933@gmail.com>
2024-10-31 15:33:30 +0600

    get keys by built in js function

--- js/test.js
+++ js/test.js
@@ -1,5 +1,7 @@
-const numbers = [12, 14, 16];
-const sum = numbers.reduce((prev, currentValue, currentIndex, array) => {
-        return prev + currentValue;
-}, 0);
-console.log(sum);
+const myObj = {
+        name: 'opu',
+        age: 26,
+        estd: 'hero'
+};
+const result = Object.keys(myObj);
+console.log(result);
