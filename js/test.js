372d09fe2fb4334fd02adcfab9760a4b8cbd47b8
zayed opu <zayedopu4933@gmail.com>
2024-10-31 12:39:05 +0600

    map  method example

--- js/test.js
+++ js/test.js
@@ -1,3 +1,5 @@
 const numbers = [2, 4, 6, 8];
-const result = numbers.push(12, 14);
-console.log('result = ',result);
+const result = numbers.map((num) => {
+    return num * 2;
+});
+console.log(result);
