becc4df92c17362ba99c8c92d8c51c23daf592c2
zayed opu <zayedopu4933@gmail.com>
2024-10-31 10:44:06 +0600

    filter example

--- js/test.js
+++ js/test.js
@@ -1,17 +1,5 @@
-class Student{
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-    }
-    test(){
-        console.log('hello');
-    }
-    exampleFunction(){
-        let array = [1, 2, 3];
-        array.find(function(){
-            this.test();
-        }, this)
-    }
-}
-let student = new Student('opu',26);
-student.exampleFunction();
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+const result = numbers.filter((currentValue, currentIndex, array) => {
+return currentValue > 4;
+});
+console.log(result);
